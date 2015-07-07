e070016286f6c54ae37bc662e59da1008b46f040
Nam Nguyen <ntgn81@gmail.com>
2015-07-06 21:53:57 -0400

    Renamed to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var cors_proxy = require('cors-anywhere');
 var host = '127.0.0.1';
-var port = 8000;
+var port = process.env.PORT || 8000;
 
 cors_proxy.createServer().listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
